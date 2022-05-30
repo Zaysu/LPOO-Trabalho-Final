214786c3db0efe0bcd20aec833cf01530da3ca61
MarceloGoncalvesBraga <85029465+MarceloGoncalvesBraga@users.noreply.github.com>
2022-05-30 15:24:07 -0300

    Update acervo.py

--- app/model/acervo.py
+++ app/model/acervo.py
@@ -1,4 +1,4 @@
-listadelivros = [{'Livro' : 'Senhor Dos Aneis', 'Autor' : 'J.R.R. Tolkien', 'ISBN' : '123456789', 'Edicao' : '1', 'Editora' : 'DarkSide','Emprestimo':'27/04/2022'},]
+listadelivros = [{'Livro' : 'Senhor Dos Aneis', 'Autor' : 'J.R.R. Tolkien', 'ISBN' : '123456789', 'Edicao' : '1', 'Editora' : 'DarkSide','Emprestimo':'1'},]
 #listadelivros = [{'Livro' : 'Senhor', 'Autor' : 'J.R.R. Tolkien', 'ISBN' : '123456789', 'Edicao' : '1', 'Editora' : 'Side','Emprestimo':'27/03/2022'},]
 listadeexemplares = [{'Nome' : 'Senhor Dos Aneis', 'Quantidade' : '2'},]
 listadeassuntos = [{'Assunto' : 'Aventura'},]
